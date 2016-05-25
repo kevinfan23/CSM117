5f713cebbaa1bc8f615edbe9001ce296666860c6
lwei0402 <likai.wei0402@gmail.com>
2016-05-24 23:13:26 -0700

    Add SubmitOption

---
+++ Clicker/Clicker/Definitions.swift
@@ -0,0 +1,17 @@
+//
+//  Definitions.swift
+//  Clicker
+//
+//  Created by Likai Wei on 5/24/16.
+//  Copyright © 2016 CS M117. All rights reserved.
+//
+
+
+import CoreBluetooth
+
+let TRANSFER_SERVICE_UUID = "E20A39F4-73F5-4BC4-A12F-17D1AD666661"
+let TRANSFER_CHARACTERISTIC_UUID = "08590F7E-DB05-467E-8757-72F6F66666D4"
+let NOTIFY_MTU = 20
+
+let transferServiceUUID = CBUUID(string: TRANSFER_SERVICE_UUID)
+let transferCharacteristicUUID = CBUUID(string: TRANSFER_CHARACTERISTIC_UUID)
